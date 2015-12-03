f6e9754f098af453d8d54a57bb0ca1af1af1fb93
madbizarre <andrew-cool@bk.ru>
2015-12-03 19:51:59 +0300

    change work

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,62 @@
-;(function(){
-    $('form').on('submit', function(e){
-       e.preventDefault();
+;(function (){
+    $(document).ready(function (){
+        var $btnSubmit = $('button[type=submit]'),
+            $contact = $('#contact'),
+            $contactTitle = $('.contact-title'),
+            $contactModal = $('#contactModal'),
+            alert = $('.alert-danger');
+        var closeAlert = function (){
+                var alert = $('.alert-danger');
+                if (alert.length) {
+                    alert.alert('close');
+                }
+            },
+            ajaxSuccess = function (data){
+                //try{}catch(e){}
+                $btnSubmit.button('reset');
+                closeAlert();
+                $contactTitle.html('Спасибо, заявка отправлена');
+                $contact.before('<div class="alert alert-success" role="alert"><strong>Мы свяжемся с Вами в ближайшее время.</strong></div>');
+                $contact.css('display', 'none');
+            },
+            ajaxError = function (err){
+                $btnSubmit.button('reset');
+                if (alert.length) {
+                    alert.close();
+                }
+                $btnSubmit.button('reset');
+                $contact.before('<div class="alert alert-danger" role="alert"><strong>Произошла ошибка! Пожалуйста, повторите попытку.</strong></div>');
+            };
+
+        $('.btn').on('click', function (){
+            closeAlert();
+            $contactModal.modal('show');
+        });
+        $contact.on('submit', function (e){
+            $btnSubmit.button('loading');
+            e.preventDefault();
+            closeAlert();
+            if (document.all && !window.atob) {
+                if ($('#inputTel').val().length == 0) {
+                    $contact.before('<div class="alert alert-danger" role="alert"><strong>Поле телефон обязательно для заполнения!</strong></div>');
+                    $btnSubmit.button('reset');
+                    return;
+                }
+            }
+            $.ajax({
+                url: '/mail',
+                method: 'post',
+                data: {
+                    name: $('#inputName').val(),
+                    tel: $('#inputTel').val()
+                },
+                success: ajaxSuccess,
+                error: ajaxError
+            });
+        });
+        if (document.all && !window.atob) {
+            $('label[for="inputName"]').html('Имя:');
+            $('label[for="inputTel"]').html('Телефон*:');
+        }
     });
 })();
